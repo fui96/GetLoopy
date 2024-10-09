3566b35688bb131ad46e89ded96a337fbca5faaf
fui96 <kidwelqn@mail.uc.edu>
2024-10-09 14:09:57 -0400

    innit

---
+++ src/CountTo30.java
@@ -0,0 +1,9 @@
+public class CountTo30 {
+    public static void main(String[] args) {
+
+        for( int i = 0 ; i <= 30 ; i++ ) {
+            System.out.println(i);
+        }
+
+    }
+}
---
+++ src/Task6.java
@@ -0,0 +1,11 @@
+public class Task6 {
+    public static void main(String[] args) {
+
+        for( int row = 5; row > 0; row--) {
+            for(int col = 0; col < row; col++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
